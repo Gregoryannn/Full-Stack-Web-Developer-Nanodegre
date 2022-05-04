7a86b36edbf038aa8e29097c434f8e87ed0a8d3b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-04 15:13:33 +0100

    PEP8 check

--- P2 - Trivia API/coffeeshop/backend/models.py
+++ P2 - Trivia API/coffeeshop/backend/models.py
@@ -18,30 +18,36 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
+
 '''
 Question
 '''
 class Question(db.Model):  
   __tablename__ = 'questions'
+
   id = Column(Integer, primary_key=True)
   question = Column(String)
   answer = Column(String)
   category = Column(String)
   difficulty = Column(Integer)
+
   def __init__(self, question, answer, category, difficulty):
     self.question = question
     self.answer = answer
     self.category = category
     self.difficulty = difficulty
+
   def insert(self):
     db.session.add(self)
     db.session.commit()
   
   def update(self):
     db.session.commit()
+
   def delete(self):
     db.session.delete(self)
     db.session.commit()
+
   def format(self):
     return {
       'id': self.id,
@@ -50,15 +56,19 @@ class Question(db.Model):
       'category': self.category,
       'difficulty': self.difficulty
     }
+
 '''
 Category
 '''
 class Category(db.Model):  
   __tablename__ = 'categories'
+
   id = Column(Integer, primary_key=True)
   type = Column(String)
+
   def __init__(self, type):
     self.type = type
+
   def format(self):
     return {
       'id': self.id,
